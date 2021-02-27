70d38f632a618d0db9714e1bdcdb48481a8cc900
Zero <69114010+Anomaliii@users.noreply.github.com>
2021-02-27 14:10:27 +0700

    Update config.py

--- lynda/config.py
+++ lynda/config.py
@@ -1,14 +1,4 @@
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
-import json
-import os
-
-
-def get_user_list(config, key):
-    with open('{}/lynda/{}'.format(os.getcwd(), config), 'r') as json_file:
-        return json.load(json_file)[key]
-
-
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = False
 
